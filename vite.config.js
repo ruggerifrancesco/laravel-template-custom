c6783ff429b8cf71f1af03b68e96be5fbffd0cc3
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-19 15:15:44 +0200

    updated vite in to receiving scss

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,18 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
+        laravel({
+            input: [
+            'resources/scss/app.scss',
             'resources/js/app.js',
-        ]),
+        ],
+        refresh: true
+    }),
     ],
+
+    resolve: {
+        alias: {
+            '~resources' : '/resources/',
+        }
+    }
 });
